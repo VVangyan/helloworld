7041640721f9dd4f575612b4d28993bba1349f19
yan <yan>
2017-10-25 15:17:52 +0800

    args

---
+++ src/main/java/com/atwy/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.atwy.helloworld;
+
+/**
+ * @author:yan
+ * @date :2017年10月25日
+ * @desc :HelloWorld
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		PrintHelloWorld.helloWorld("Hello world!");
+	}
+}
+
+class PrintHelloWorld {
+	public static void helloWorld(String str) {
+		System.out.println(str);
+	}
+}
