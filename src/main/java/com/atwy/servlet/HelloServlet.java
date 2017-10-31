f2e79a5ef6ac032f9556d9642650cb7a1ae33555
yan <yan>
2017-10-31 14:53:17 +0800

    修改测试

--- src/main/java/com/atwy/servlet/HelloServlet.java
+++ src/main/java/com/atwy/servlet/HelloServlet.java
@@ -28,9 +28,6 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("from  hello Served at: ").append(request.getContextPath());
-	
-	
-	
 	}
 
 	/**
